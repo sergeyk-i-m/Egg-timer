e3ea947bb1c177c1a8a94d35d5ffdd088d8bc83d
Sergey Kim <97921237+sergeyk-i-m@users.noreply.github.com>
2025-07-17 20:35:41 -0500

    Add files via upload

---
+++ Hard/hard.js
@@ -0,0 +1,32 @@
+let timer = 480
+const tiktak = document.querySelector('#tikTak');
+const dindon = new Audio();
+dindon.src = 'Din don.mp3';
+
+function calculateTime() {
+    const countdown = document.querySelector('#countdown');
+    let minutes = Math.floor(timer / 60);
+    let seconds = timer % 60;
+
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+
+    countdown.textContent = `${minutes} : ${seconds}`;
+    timer--;
+
+    if (timer < 0) {
+        stopTimer();
+        timer = 0
+        tiktak.pause();
+        dindon.play();
+    }
+
+    function stopTimer() {
+        clearInterval(timerId);
+    }
+
+}
+
+let timerId = setInterval(calculateTime, 1000);
+
